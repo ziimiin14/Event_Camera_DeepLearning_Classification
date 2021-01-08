0931033e49145846187aadbea1e59fb61b91aba9
ziimiin14 <erzimin@gmail.com>
2021-01-08 16:28:45 +0800

    One class SVM

--- scripts/image_data_augmentation.py
+++ scripts/image_data_augmentation.py
@@ -1,10 +1,11 @@
 import numpy as np
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.applications.resnet50 import preprocess_input
 import os
+from sklearn.model_selection import train_test_split
 
-image_size = 224
+image_size = 150
 def read_and_prep_images(img_paths, img_height=image_size, img_width=image_size):
     imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
     img_array = np.array([img_to_array(img) for img in imgs])
@@ -19,3 +20,4 @@ x = read_and_prep_images(a)
 # x = (x-x.min())/(x.max()-x.min())
 # print(x.min())
 # print(x.max())
+
--- scripts/images_data_filter_rename.py
+++ scripts/images_data_filter_rename.py
@@ -1,35 +1,42 @@
 import os
 from shutil import copyfile
 
-des_path = os.path.join(os.curdir,'mannequin_filter')
+des_path = os.path.join(os.curdir,'not_mannequin_filter')
 
 if os.path.exists(des_path):
     pass
 else:
     os.mkdir(des_path)
 
-src_path = os.path.join(os.curdir,'mannequin')
-src_path1 = os.path.join(os.curdir,'mannequin1')
 
-
-images_name = os.listdir(src_path)
-images_name1 = os.listdir(src_path1)
+src_path = os.path.join(os.curdir,'not_mannequin')
 
 count = 0
 
-for x in images_name:
-    src_temp = os.path.join(src_path,x)
-    des_temp = os.path.join(des_path,str(count)+'.jpg')
-    count += 1
-    copyfile(src_temp,des_temp)
-    # print(des_temp)
 
-for y in images_name1:
-    src_temp1 = os.path.join(src_path1,y)
-    des_temp1 = os.path.join(des_path,str(count)+'.jpg')
-    count += 1
-    copyfile(src_temp1,des_temp1)
-    # print(des_temp1)
+for x in os.listdir(src_path):
+    temp_dir = os.path.join(src_path,x)
+    images_name = os.listdir(temp_dir)
+    for y in images_name:
+        src_temp = os.path.join(temp_dir,y)
+        des_temp = os.path.join(des_path,str(count)+'.jpg')
+        count += 1
+        copyfile(src_temp,des_temp)
+
+
+# for x in images_name:
+#     src_temp = os.path.join(src_path,x)
+#     des_temp = os.path.join(des_path,str(count)+'.jpg')
+#     count += 1
+#     copyfile(src_temp,des_temp)
+#     # print(des_temp)
+
+# for y in images_name1:
+#     src_temp1 = os.path.join(src_path1,y)
+#     des_temp1 = os.path.join(des_path,str(count)+'.jpg')
+#     count += 1
+#     copyfile(src_temp1,des_temp1)
+#     # print(des_temp1)
 
 
 
