ba36052efe8cf53307a790aad83cff7b7407f83d
ziimiin14 <erzimin@gmail.com>
2021-01-11 17:04:13 +0800

    DNN training for manniquin

--- scripts/images_data_filter_rename.py
+++ scripts/images_data_filter_rename.py
@@ -1,7 +1,7 @@
 import os
 from shutil import copyfile
 
-des_path = os.path.join(os.curdir,'not_mannequin_filter')
+des_path = 'eventsData/training/not_mannequin'
 
 if os.path.exists(des_path):
     pass
@@ -9,7 +9,9 @@ else:
     os.mkdir(des_path)
 
 
-src_path = os.path.join(os.curdir,'not_mannequin')
+src_path = os.path.join(os.curdir,'nohuman2')
+src_path1 = os.path.join(os.curdir,'nohuman2_noisy')
+
 
 count = 0
 
@@ -19,9 +21,20 @@ for x in os.listdir(src_path):
     images_name = os.listdir(temp_dir)
     for y in images_name:
         src_temp = os.path.join(temp_dir,y)
-        des_temp = os.path.join(des_path,str(count)+'.jpg')
+        des_temp = os.path.join(des_path,str(count)+'.png')
         count += 1
         copyfile(src_temp,des_temp)
+
+
+for x in os.listdir(src_path1):
+    temp_dir = os.path.join(src_path1,x)
+    images_name = os.listdir(temp_dir)
+    for y in images_name:
+        src_temp = os.path.join(temp_dir,y)
+        des_temp = os.path.join(des_path,str(count)+'.png')
+        count += 1
+        copyfile(src_temp,des_temp)
+
 
 
 # for x in images_name:
