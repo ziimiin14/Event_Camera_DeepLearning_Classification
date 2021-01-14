36df1b4f3171cd6a71404078421a97546b5f6cb3
ziimiin14 <erzimin@gmail.com>
2021-01-14 16:56:47 +0800

    modified aggregation gaussian mapping and wrote a synthesized event frames script

--- scripts/aggregation_mapping.py
+++ scripts/aggregation_mapping.py
@@ -3,25 +3,6 @@
 import numpy as np
 
 
-
-def cal_mg(points,value):
-    rows = 246
-    cols = 326
-    events_img = np.zeros((rows,cols))
-    
-    for i in range(points.shape[0]):
-
-        hor = points[i,0]
-        ver = points[i,1]
-        
-
-        events_img[ver:ver+7,hor:hor+7] = events_img[ver:ver+7,hor:hor+7] + value
-    
-    
-    events_img = events_img[3:rows-3,3:cols-3]
-    return events_img
-
-
 ## Calculate the gradient value and store it for future use
 points =np.array((150,150))
 points = points.reshape(1,-1)
@@ -38,12 +19,33 @@ val = amplitude* np.exp(-exponent)
 val = val.reshape(7,7)
 
 
-## Convert x,y pixel into (n,2) array
-x = np.array([[0,0],[190,190],[319,239]])
+def cal_mg(points,value=val):
+    rows = 246
+    cols = 326
+    events_img = np.zeros((rows,cols))
+    
+    for i in range(points.shape[0]):
+
+        hor = points[i,0]
+        ver = points[i,1]
+        
+
+        events_img[ver:ver+7,hor:hor+7] = events_img[ver:ver+7,hor:hor+7] + value
+    
+    
+    events_img = events_img[3:rows-3,3:cols-3]
+    events_img = events_img/events_img.max()
+    return events_img
 
 
-## For each 10ms calculate the image with weight gradient and save it
-img = cal_mg(x,val)
+
+
+# ## Convert x,y pixel into (n,2) array
+# x = np.array([[0,0],[190,190],[319,239]])
+
+
+# ## For each 10ms calculate the image with weight gradient and save it
+# img = cal_mg(x,val)
 
 
 
