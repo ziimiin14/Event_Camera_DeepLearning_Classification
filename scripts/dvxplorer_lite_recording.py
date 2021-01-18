d9da88d6cafd0105e8605e99b1a993591776af07
ziimiin14 <erzimin@gmail.com>
2021-01-18 17:39:55 +0800

    dvxplorer script using pyaer

--- scripts/dvxplorer_lite_recording.py
+++ scripts/dvxplorer_lite_recording.py
@@ -38,8 +38,12 @@ device.start_data_stream()
 # load new config
 device.set_bias_from_json("./scripts/configs/dvxplorer_config.json")
 
-print(device.get_bias())
 
+ignore_pixel_set = [(174,154),(152,169),(92,19),(135,0),(108,178),(238,127),(182,294),(155,133)]
+
+cc = 0
+print(device.get_bias())
+count = 0
 while True:
     try:
         (pol_events, num_pol_event,
@@ -47,17 +51,19 @@ while True:
          imu_events, num_imu_event) = \
             device.get_event("events_hist")
 
-        print("Number of events:", num_pol_event)
-
         if num_pol_event != 0:
-            # img = pol_events[..., 1]-pol_events[..., 0]
-            img = pol_events[...,0]
-            # img = np.clip(img, -clip_value, clip_value)
-            # img = (img+clip_value)/float(clip_value*2)
-            img = img/img.max()
 
+            img = pol_events[...,1]
+
+            for x in ignore_pixel_set:
+                img[x] = 0
+            
+            a = img.max()/2
+            img = img/a
+            
             cv2.imshow("image", img)
-
+            cv2.imwrite('./synthetic_event_frame/mannequin'+str(count)+'.png',img*255)
+            count += 1
             cv2.waitKey(1)
 
     except KeyboardInterrupt:
